ca3bfe33188dcf391a0a8b0e0895711e11ec3bdc
Mohammad <radiophp@gmail.com>
2026-06-14 22:50:27 +0330

    chore: remove stale dev artifacts from original repo

--- source/test_vulnerabilities.sh
+++
@@ -1,4 +0,0 @@
-curl -s -X POST 'http://localhost:37777/save' \
--H 'Content-Type: application/json' \
--d '{"title":"TEST","narrative":"TEST","project":null,"observation_type":"bugfix","noise_level":"low","facts":["string1", "string2"],"concepts":[]}'
-echo ""
